b6e38dded3553fff5ade87d866c628bcf4bc95e7
Menat Pink <pink@Pinks-MacBook-Pro.local>
2021-12-05 02:18:21 +0000

    Additional styling and hard coded values

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,9 +9,9 @@ const Card = () => {
 		<div className="card">
 			<div className="card-inner">
 				<div className="card-content">
-					<div className="mt-1 flex-between center-align">
+					<div className="mt-2 flex-between center-align">
 						<p>Work</p>
-						<img src="/icon-ellipsis.svg" alt="" />
+						<img src="/images/icon-ellipsis.svg" alt="" />
 					</div>
 					<h1 className = "font-light">32hrs</h1>
 					<p className = "font-medium mb-3 mt-1 card-caption">Last Week - 36hrs</p>
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import  '../styles/components/ProfileCard.scss';
+import '../styles/components/ProfileCard.scss';
 
 const ProfileCard = () => {
 	return (
 		<div className="profile-card">
-			<img src="" alt="" />
-			<p>report for</p>
-			<h2>Jeremy Robson</h2>
-			<ul>
-				<li>Daily</li>
-				<li>Weekly</li>
-				<li>Monthly</li>
-			</ul>
+			<div className="profile-card-inner">
+				<div className="profile-card-content">  
+					<img src="/images/image-jeremy.png" alt="" />
+					<p className = "font-medium mt-2">report for</p>
+					<h1 className = "font-light">Jeremy Robson</h1>
+				</div>
+			</div>
+					<ul className = "time-selector-container">
+						<li>Daily</li>
+						<li>Weekly</li>
+						<li>Monthly</li>
+					</ul>
 		</div>
 	);
 };
